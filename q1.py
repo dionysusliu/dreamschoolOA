71f123885d1fa14630ab9a2289c077a5175d5414
chuang <chuangliu0815@gmail.com>
2024-05-02 22:37:26 -0400

    q1

--- q1.py
+++ q1.py
@@ -1,25 +1,27 @@
 
 def findMinSequenceCount(source: str, target: str):
-    m, n = len(source), len(target)
-    dp = [[float('inf') * (n + 1) for _ in range(m + 1)]]
+    def longestSub(i, j):
+        while i < n and j < m:
+            if source[i] == target[j]:
+                j += 1
+            i += 1
+        return j
 
-    # base cases
-    for i in range(m + 1): # empty target
-        dp[i][0] = 0 
-    for j in range(1, n + 1): # empty source
-        dp[0][j] = float('inf')
-    
-    for i in range(1, m + 1):
-        for j in range(1, n + 1):
-            if source[i-1] == target[i-1]:
-                dp[i][j] = dp[i-1][j-1]
-            else:
-                dp[i][j] = 1 + min(dp[i-1][j], dp[i][j-1])
-
-    return dp[m][n] if dp[m][n] != float('inf') else -1
+    n, m = len(source), len(target)
+    res = j = 0
+    while j < m:
+        t = longestSub(0, j)
+        if t == j:
+            return -1
+        j = t
+        res += 1
+    return res
 
 
 def main():
     print(findMinSequenceCount("abc", "abcbc"))
     print(findMinSequenceCount("abc", "acdbc"))
-    print(findMinSequenceCount("xyz", "xyzxz"))
+    print(findMinSequenceCount("xyz", "xzyxz"))
+
+if __name__ == '__main__':
+    main()
